d1b8015b9f542c62e6aafeb8d5d54c4ea55de3d0
kurigaki <kenji.kurigaki@gmail.com>
2022-06-25 00:13:46 +0900

    編集機能を実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -21,9 +21,17 @@ class PrototypesController < ApplicationController
   end
 
   def edit
+    @prototype = Prototype.find(params[:id])
   end
 
   def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+      redirect_to prototype_path(prototype.id)
+    else
+      @prototype = Prototype.find(params[:id])
+      render :edit
+    end
   end
   
   private
